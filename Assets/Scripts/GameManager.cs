c66242af30b4891007857cc9bd7e0be7dd262cff
Jeremy <holdemplease@hotmail.com>
2015-03-17 10:17:57 +0900

    sets us up on git

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance = null;
+	public BoardManager boardScript;
+
+	private int level = 3;
+
+	// Use this for initialization
+	void Awake ()
+	{
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy (gameObject);
+
+		// ordinarily the hierarchy of objects is destroyed when new scenes are loaded
+		// we want the GameManager singleton to persist between scenes to keep track of score, etc.
+		DontDestroyOnLoad (gameObject);
+
+		boardScript = GetComponent<BoardManager> ();
+		InitGame ();
+	}
+
+	void InitGame()
+	{
+		boardScript.SetupScene (level);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
